1cb0111356bb589d9eea1b18f3f962241b3c7779
Paulo Mujollo <paulinhomujollo@gmail.com>
2023-02-13 12:37:13 -0300

    teste inicial

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição Mentoria Java");
+        mentoria1.setData(LocalDate.now());
+        System.out.println(mentoria1);
+
+
+    }
+}
---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,56 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.Objects;
+
+public class Curso {
+
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Curso curso = (Curso) o;
+        return cargaHoraria == curso.cargaHoraria && Objects.equals(titulo, curso.titulo) && Objects.equals(descricao, curso.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo, descricao, cargaHoraria);
+    }
+}
